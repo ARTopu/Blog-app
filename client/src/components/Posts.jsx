10d398caf47560fee2911c574c35925b8f9f6371
Atiqur Rahman <atiqurrahman2312002@gmail.com>
2023-12-03 12:15:45 +0600

    postitem update

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -4,6 +4,7 @@ import Thumbnail1 from '../images/blog1.jpg'
 import Thumbnail2 from '../images/blog2.jpg'
 import Thumbnail3 from '../images/blog3.jpg'
 import Thumbnail4 from '../images/blog4.jpg'
+import PostItem from './PostItem'
 
 const DUMMY_POSTS = [
   {
@@ -11,7 +12,7 @@ const DUMMY_POSTS = [
     thumbnail: Thumbnail1,
     category:'education',
     title:'This is the title of the very 1st post on this blog.',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem odit quae quas! Est tempora porro, suscipit fuga non deleniti aliquid voluptate quo corporis vero sapiente eligendi ea sunt ratione fugit molestias veritatis soluta illo fugiat itaque at optio qui! Quam aspernatur cupiditate ea delectus quis necessitatibus sint blanditiis a itaque odio fuga, quibusdam aperiam quod quaerat exercitationem, inventore officiis quo dolorum sequi. Corrupti totam saepe cumque eaque ex facilis voluptatum placeat soluta iure officia deserunt excepturi tenetur ut, blanditiis quasi, illum sequi magni esse at a aut laborum quaerat. Suscipit facilis minus provident doloribus quia unde cum eos optio excepturi.'
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem odit quae quas! Est tempora porro, suscipit fuga non deleniti aliquid voluptate quo corporis vero sapiente eligendi ea sunt ratione fugit molestias veritatis soluta illo fugiat itaque at optio qui! Quam aspernatur cupiditate ea delectus quis necessitatibus sint blanditiis a itaque odio fuga, quibusdam aperiam quod quaerat exercitationem, inventore officiis quo dolorum sequi. Corrupti totam saepe cumque eaque ex facilis voluptatum placeat soluta iure officia deserunt excepturi tenetur ut, blanditiis quasi, illum sequi magni esse at a aut laborum quaerat. Suscipit facilis minus provident doloribus quia unde cum eos optio excepturi.',
     authorID: 3
   },
   {
@@ -19,7 +20,7 @@ const DUMMY_POSTS = [
     thumbnail: Thumbnail2,
     category:'science',
     title:'This is the title of the very 2nd post on this blog.',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem odit quae quas! Est tempora porro, suscipit fuga non deleniti aliquid voluptate quo corporis vero sapiente eligendi ea sunt ratione fugit molestias veritatis soluta illo fugiat itaque at optio qui! Quam aspernatur cupiditate ea delectus quis necessitatibus sint blanditiis a itaque odio fuga, quibusdam aperiam quod quaerat exercitationem, inventore officiis quo dolorum sequi. Corrupti totam saepe cumque eaque ex facilis voluptatum placeat soluta iure officia deserunt excepturi tenetur ut, blanditiis quasi, illum sequi magni esse at a aut laborum quaerat. Suscipit facilis minus provident doloribus quia unde cum eos optio excepturi.'
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem odit quae quas! Est tempora porro, suscipit fuga non deleniti aliquid voluptate quo corporis vero sapiente eligendi ea sunt ratione fugit molestias veritatis soluta illo fugiat itaque at optio qui! Quam aspernatur cupiditate ea delectus quis necessitatibus sint blanditiis a itaque odio fuga, quibusdam aperiam quod quaerat exercitationem, inventore officiis quo dolorum sequi. Corrupti totam saepe cumque eaque ex facilis voluptatum placeat soluta iure officia deserunt excepturi tenetur ut, blanditiis quasi, illum sequi magni esse at a aut laborum quaerat. Suscipit facilis minus provident doloribus quia unde cum eos optio excepturi.',
     authorID: 1
   },
   {
@@ -27,7 +28,7 @@ const DUMMY_POSTS = [
     thumbnail: Thumbnail3,
     category:'weather',
     title:'This is the title of the very 3rd post on this blog.',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem odit quae quas! Est tempora porro, suscipit fuga non deleniti aliquid voluptate quo corporis vero sapiente eligendi ea sunt ratione fugit molestias veritatis soluta illo fugiat itaque at optio qui! Quam aspernatur cupiditate ea delectus quis necessitatibus sint blanditiis a itaque odio fuga, quibusdam aperiam quod quaerat exercitationem, inventore officiis quo dolorum sequi. Corrupti totam saepe cumque eaque ex facilis voluptatum placeat soluta iure officia deserunt excepturi tenetur ut, blanditiis quasi, illum sequi magni esse at a aut laborum quaerat. Suscipit facilis minus provident doloribus quia unde cum eos optio excepturi.'
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem odit quae quas! Est tempora porro, suscipit fuga non deleniti aliquid voluptate quo corporis vero sapiente eligendi ea sunt ratione fugit molestias veritatis soluta illo fugiat itaque at optio qui! Quam aspernatur cupiditate ea delectus quis necessitatibus sint blanditiis a itaque odio fuga, quibusdam aperiam quod quaerat exercitationem, inventore officiis quo dolorum sequi. Corrupti totam saepe cumque eaque ex facilis voluptatum placeat soluta iure officia deserunt excepturi tenetur ut, blanditiis quasi, illum sequi magni esse at a aut laborum quaerat. Suscipit facilis minus provident doloribus quia unde cum eos optio excepturi.',
     authorID: 13
   },
   {
@@ -35,7 +36,7 @@ const DUMMY_POSTS = [
     thumbnail: Thumbnail4,
     category:'farming',
     title:'This is the title of the very 4th post on this blog.',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem odit quae quas! Est tempora porro, suscipit fuga non deleniti aliquid voluptate quo corporis vero sapiente eligendi ea sunt ratione fugit molestias veritatis soluta illo fugiat itaque at optio qui! Quam aspernatur cupiditate ea delectus quis necessitatibus sint blanditiis a itaque odio fuga, quibusdam aperiam quod quaerat exercitationem, inventore officiis quo dolorum sequi. Corrupti totam saepe cumque eaque ex facilis voluptatum placeat soluta iure officia deserunt excepturi tenetur ut, blanditiis quasi, illum sequi magni esse at a aut laborum quaerat. Suscipit facilis minus provident doloribus quia unde cum eos optio excepturi.'
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem odit quae quas! Est tempora porro, suscipit fuga non deleniti aliquid voluptate quo corporis vero sapiente eligendi ea sunt ratione fugit molestias veritatis soluta illo fugiat itaque at optio qui! Quam aspernatur cupiditate ea delectus quis necessitatibus sint blanditiis a itaque odio fuga, quibusdam aperiam quod quaerat exercitationem, inventore officiis quo dolorum sequi. Corrupti totam saepe cumque eaque ex facilis voluptatum placeat soluta iure officia deserunt excepturi tenetur ut, blanditiis quasi, illum sequi magni esse at a aut laborum quaerat. Suscipit facilis minus provident doloribus quia unde cum eos optio excepturi.',
     authorID: 11
   },
 ]
